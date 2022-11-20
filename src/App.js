b4a3779aa43c4abab468beb04a6d186fbb2a48c8
Drew Brown <abrown5@ufl.edu>
2022-11-20 18:11:40 -0500

    updated name to Drew

--- src/App.js
+++ src/App.js
@@ -5,19 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
-        <p> My name is Sami and this is my project </p>
-=======
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! This is our team project.
-        </p>	
-<<<<<<< HEAD
->>>>>>> 242861e (added new project text)
-=======
->>>>>>> 242861ea9faa50ad2a4d6c5582313d2d2fb703ef
+        <p> My name is Drew and this is my project </p>
       </header>
     </div>
   );
